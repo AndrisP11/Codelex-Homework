233969a06a0216c888a8238f97a80745092a0f2a
AndrisP11 <110040171+AndrisP11@users.noreply.github.com>
2022-08-15 14:55:07 +0300

    "Small change"

--- prep-course-warm-up-main/src/41-circle.ts
+++ prep-course-warm-up-main/src/41-circle.ts
@@ -40,7 +40,7 @@ constructor(radius: number) {
 	this.perimeter = function() {
 		return (2 * Math.PI * this.radius).toFixed(2)
 		};
-		}
+		}}
 const c = new Circle(3);
 console.log("Area =", c.area()); // Expected output: Area = 28.27
 console.log("Perimeter =", c.perimeter()); // Expected output: Perimeter = 18.85
